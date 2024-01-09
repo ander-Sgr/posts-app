f03791985d6a47b01bdbaa0c64aeb0a2ce07c18e
Anderson <andersonbarreto2001@gmail.com>
2024-01-09 12:45:22 +0100

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\RegisterController;
+use App\Models\Rol;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ViewErrorBag;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +19,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [homeController::class, 'index']);
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'showLoginView')->middleware('guest');
+    Route::post('/login', 'store')->middleware('guest');
+    Route::post('/logout', 'destroySession')->middleware('auth');
 });
 
-Route::get('paginacio', function(){
-    return view('usuaris.index', [
-        'usuaris' => DB::table('users')->paginate(10)
-    ]);
+Route::controller(RegisterController::class)->group(function () {
+    Route::get('/register', 'showRegisterView')->middleware('guest')->name('register');
+    Route::post('/storeUser', 'store');
 });
+
+Route::controller(PostsController::class)->group(function () {
+    Route::get('/home', 'showAllPosts')->middleware('auth');
+    Route::get('/myPosts', 'showPosts')->middleware('auth');
+    Route::get('/viewPost/{id}', 'showPost')->middleware('auth');
+    Route::get('/editPost/{id}', 'editPostView')->middleware('auth');
+    Route::get('/viewCreatePost', 'viewCreatePost')->middleware('auth');
+    Route::put('/editPost/{id}', 'updatePost');
+    Route::post('/createPost', 'createPost');
+    Route::post('/deletePost/{id}', 'deletePost');
+});
+
+// TO DO VERIFICAR ELFUNCIONAMINETO DE LA FUNCION CRATE POST 
