d7f4bc657e4e5bf198138295c0255d5643c09c5c
Anderson <andersonbarreto2001@gmail.com>
2024-01-09 12:41:57 +0100

    seeders database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Image;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -15,10 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(90)
-            ->has(Post::factory()->count(10))
-            ->create();
-        
+        /*User::factory(3)
+        ->has(Post::factory()->count(5))
+        ->create();*/
+        $posts = Post::all();
+        foreach ($posts as $post) {
+            Image::factory()->create([
+                'imageable_id' => $post->id,
+                'imageable_type' => Post::class
+            ]);
+        }
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
